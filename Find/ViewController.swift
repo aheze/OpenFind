dce428b40779b33a2464b83760fdd07b0d9bb084
John <zjohnzheng@gmail.com>
2019-10-13 11:44:36 -0700

    Initial Commit

---
+++ Find/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find
+//
+//  Created by Andrew on 10/13/19.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
