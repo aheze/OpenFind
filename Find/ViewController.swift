e6f128e462663b8a7699f3ca9de1a4eb009d769b
John <zjohnzheng@gmail.com>
2019-10-13 12:11:45 -0700

    set up project, install pods

--- Find/ViewController.swift
+++ Find/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: Sceneview
+        sceneView.delegate = self
+        let sceneConfiguration = ARWorldTrackingConfiguration()
+        sceneConfiguration.planeDetection = .horizontal
+        sceneView.session.run(sceneConfiguration)
     }
+    
 
 
+}
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIColor.white
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            node.addChildNode(planeNode)
+        }else{
+           return
+        }
+    }
+    
 }
 
