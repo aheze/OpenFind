7a32a5455bf397a8d0297dd8503f84d2984883e0
Zheng <ahzzheng@gmail.com>
2021-01-09 15:50:44 -0800

    Non-glitchy dismissal

--- Find/Tab Bar Controllers/Photos/Slides/PhotoSlidesVC+ZoomDelegate.swift
+++ Find/Tab Bar Controllers/Photos/Slides/PhotoSlidesVC+ZoomDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 extension PhotoSlidesViewController: ZoomAnimatorDelegate {
 
@@ -21,7 +22,34 @@ extension PhotoSlidesViewController: ZoomAnimatorDelegate {
     }
 
     func referenceImageViewFrameInTransitioningView(for zoomAnimator: ZoomAnimator) -> CGRect? {
-        print("trans")
-        return currentViewController.scrollView.convert(currentViewController.contentView.frame, to: currentViewController.view)
+        if
+            let imageView = currentViewController.imageView,
+            let image = imageView.image
+        {
+            let aspectFrame = AVMakeRect(aspectRatio: image.size, insideRect: imageView.frame)
+            return currentViewController.scrollView.convert(aspectFrame, to: currentViewController.view)
+        }
+        return currentViewController.scrollView.convert(currentViewController.imageView.frame, to: currentViewController.view)
+    }
+}
+
+extension UIImageView {
+    var contentClippingRect: CGRect {
+        guard let image = image else { return bounds }
+        guard contentMode == .scaleAspectFit else { return bounds }
+        guard image.size.width > 0 && image.size.height > 0 else { return bounds }
+
+        let scale: CGFloat
+        if image.size.width > image.size.height {
+            scale = bounds.width / image.size.width
+        } else {
+            scale = bounds.height / image.size.height
+        }
+
+        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
+        let x = (bounds.width - size.width) / 2.0
+        let y = (bounds.height - size.height) / 2.0
+
+        return CGRect(x: x, y: y, width: size.width, height: size.height)
     }
 }
