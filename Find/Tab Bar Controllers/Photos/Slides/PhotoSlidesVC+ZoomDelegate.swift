10999c4e734385ad1bd65b5430962f925831a63d
Zheng <ahzzheng@gmail.com>
2021-01-09 15:51:24 -0800

    Remove unused aspect fit extension

--- Find/Tab Bar Controllers/Photos/Slides/PhotoSlidesVC+ZoomDelegate.swift
+++ Find/Tab Bar Controllers/Photos/Slides/PhotoSlidesVC+ZoomDelegate.swift
@@ -32,24 +32,3 @@ extension PhotoSlidesViewController: ZoomAnimatorDelegate {
         return currentViewController.scrollView.convert(currentViewController.imageView.frame, to: currentViewController.view)
     }
 }
-
-extension UIImageView {
-    var contentClippingRect: CGRect {
-        guard let image = image else { return bounds }
-        guard contentMode == .scaleAspectFit else { return bounds }
-        guard image.size.width > 0 && image.size.height > 0 else { return bounds }
-
-        let scale: CGFloat
-        if image.size.width > image.size.height {
-            scale = bounds.width / image.size.width
-        } else {
-            scale = bounds.height / image.size.height
-        }
-
-        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
-        let x = (bounds.width - size.width) / 2.0
-        let y = (bounds.height - size.height) / 2.0
-
-        return CGRect(x: x, y: y, width: size.width, height: size.height)
-    }
-}
