92f7900fb6c84b944d3561b12572154ed093ef5a
Zheng <aheze@getfind.app>
2022-04-29 15:01:20 -0700

    Fix launch

--- Find-New/Initial/InitialViewController.swift
+++ Find-New/Initial/InitialViewController.swift
@@ -40,6 +40,10 @@ class InitialViewController: UIViewController {
             return nil
         }
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
